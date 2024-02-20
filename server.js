e2e98ace35f7c71b8040f6db87927696678b1ecf
Michael <michaelmc2005@gmail.com>
2024-02-20 01:10:32 +0000

    github

--- server.js
+++ server.js
@@ -74,5 +74,5 @@ app.post('/chat', async (req, res) => {
 
 // start the server
 app.listen(port, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running');
 });
